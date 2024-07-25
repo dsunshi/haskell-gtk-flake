29f929b85ecf0300522154553aa27e7bd3d3f25e
D. Sunshine <david@sunshines.org>
2024-07-25 02:50:44 +0000

    Bezier curves
    Simple lerp, quadratic, and cubic functions

--- src/Draw.hs
+++ src/Draw.hs
@@ -12,6 +12,44 @@ canvasWidth = 256
 canvasHeight :: Int
 canvasHeight = 256
 
+
+lerp :: Double -> -- x1
+        Double -> -- y1
+        Double -> -- x2
+        Double -> -- y2
+        Double -> -- t
+        (Double, Double)
+lerp x1 y1 x2 y2 t = (x1 + (x2 - x1) * t, y1 + (y2 - y1) * t)
+
+quadratic :: Double -> -- x1
+             Double -> -- y1
+             Double -> -- x2
+             Double -> -- y2
+             Double -> -- x3
+             Double -> -- y3
+             Double -> -- t
+             (Double, Double)
+quadratic x1 y1 x2 y2  x3 y3 t = lerp p0x p0y p1x p1y t
+    where
+        (p0x, p0y) = lerp x1 y1 x2 y2 t
+        (p1x, p1y) = lerp x2 y2 x3 y3 t
+
+cubic :: Double -> -- x1
+         Double -> -- y1
+         Double -> -- x2
+         Double -> -- y2
+         Double -> -- x3
+         Double -> -- y3
+         Double -> -- x4
+         Double -> -- y4
+         Double -> -- t
+         (Double, Double)
+cubic x1 y1 x2 y2  x3 y3  x4 y4 t = lerp p0x p0y p1x p1y t
+    where
+        (p0x, p0y) = quadratic x1 y1 x2 y2 x3 y3 t
+        (p1x, p1y) = quadratic x2 y2 x3 y3 x4 y4 t
+
+
 drawCanvas :: Gtk.IsWidget widget => widget -> Double -> Double -> Render ()
 drawCanvas _canvas width height = do
   save
