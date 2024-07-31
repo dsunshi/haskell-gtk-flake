fcbbf951d47a9703440776169056a2b01108fcab
D. Sunshine <david@sunshines.org>
2024-07-31 01:35:43 +0000

    Perlin noise
    - Library for perlin noise
    - New constructor for Color to allow easier grayscale 0 .. 1

--- app/Draw.hs
+++ app/Draw.hs
@@ -6,12 +6,23 @@ import GI.Cairo.Render hiding (x, y, width, height)
 import Draw.Shape
 import Draw.Color
 import Draw.Path
+import Draw.Noise
 
 canvasWidth :: Int
-canvasWidth = 256
+canvasWidth = 400
 
 canvasHeight :: Int
-canvasHeight = 256
+canvasHeight = 400
+
+perlin :: Double -> Double -> Render ()
+perlin x y = do
+    color $ Gray ((1.0 + noise x y 0) / 2.0) -- [-1 .. 1] -> [0 .. 1]
+    point x y
+
+canvasPoints :: [(Double, Double)]
+canvasPoints = [(x, y)
+  | x <- [1.0 .. fromIntegral canvasWidth]
+  , y <- [1.0 .. fromIntegral canvasHeight] ]
 
 drawCanvas :: Gtk.IsWidget widget => widget -> Double -> Double -> Render ()
 drawCanvas _canvas width height = do
@@ -22,12 +33,13 @@ drawCanvas _canvas width height = do
 
   color $ Hex 0x2e3440
   rect 10 10 (width - 20) (height - 20)
-
   color $ Hex 0x4c566a
   setLineWidth 1
   bezier 0 0 12 76 97 48 100 100
 
   circle 200 200 10
   point 220 220
+
+  mapM_ (uncurry perlin) canvasPoints
 
   restore
